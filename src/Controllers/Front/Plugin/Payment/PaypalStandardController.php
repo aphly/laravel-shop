e15a2f0a733ccc82b006daebb5109dcbb1653ec8
Aphly <121099327@qq.com>
2022-06-02 17:17:47 +0800

    update

---
+++ src/Controllers/Front/Plugin/Payment/PaypalStandardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aphly\LaravelShop\Controllers\Front\Plugin\Payment;
+
+use Aphly\Laravel\Exceptions\ApiException;
+use Aphly\LaravelShop\Controllers\Front\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class PaypalStandardController extends Controller
+{
+
+    public function index(Request $request)
+    {
+
+    }
+
+
+}
+
